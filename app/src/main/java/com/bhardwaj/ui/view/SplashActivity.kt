a154ee6c90c5cb837825f98c856b25255e4fde80
Aditya Bhardwaj <ab2225@srmist.edu.in>
2021-08-20 01:57:13 +0530

    Splash Screen Added.

---
+++ app/src/main/java/com/bhardwaj/ui/view/SplashActivity.kt
@@ -0,0 +1,20 @@
+package com.bhardwaj.ui.view
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import com.bhardwaj.ui.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        Handler(Looper.myLooper()!!).postDelayed({
+            startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+            finish()
+        }, 5000)
+    }
+}
